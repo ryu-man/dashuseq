8f0a60b7594e82e25252f950b9b4620269874577
ryu-man <rioman19@gmail.com>
2021-01-17 19:57:23 +0100

    fix: set ssl to true

--- model/index.js
+++ model/index.js
@@ -3,11 +3,8 @@ const { Sequelize } = require("sequelize")
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: "postgres",
-    ssl: true,
     dialectOptions: {
-        ssl: {
-            rejectUnauthorized: false
-        }
+        ssl:true
     }
 })
 
